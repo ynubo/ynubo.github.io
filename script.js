b7f0582d4e258570a667ee1a9bf484dd2eeefefd
ynubo <heartbreakexe00001@gmail.com>
2026-09-25 22:55:27 +0300

    all remake site

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", event => {
+    const target = document.querySelector(link.getAttribute("href"));
+    if (!target) return;
+    event.preventDefault();
+    target.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+});
